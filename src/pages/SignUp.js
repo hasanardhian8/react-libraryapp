c74457cec26af3199ae82886afa3d401b5dda5da
hasanardhian8 <hasanardhian11@gmail.com>
2022-11-02 23:04:25 +0700

    .

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SignUp = () => {
+
+  const [values, setValues] = useState({
+    name : " ",
+    email : " ",
+    password : " "
+  });
+
+  const (name,email, password)=values;
+
   return (
     <div>
       <form>
         <div>
           <label>username</label>
+          <input id="username" type="text" placeholder="Username"></input>
         </div>
         <div>
           <label>email</label>
+          <input id="email" type="email" placeholder="E-mail"></input>
         </div>
         <div>
           <label>password</label>
+          <input id="password" type="text" placeholder="********"></input>
+        </div>
+        <div>
+          <button type="submit">register</button>
         </div>
       </form>
     </div>
