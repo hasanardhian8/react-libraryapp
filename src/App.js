9e1a1641c3d09bbb0f4ca21f39fa462ce12a10f7
hasanardhian8 <hasanardhian11@gmail.com>
2022-11-02 07:54:20 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
